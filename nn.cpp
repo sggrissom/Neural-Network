118fad5676dfda5c8d2dc3827358a1986521679e
Steven Grissom <sggrissom@gmail.com>
2015-09-02 17:01:03 -0500

    nn progess, working on initialize the net with random weight values. still just laying down framework.

--- nn.cpp
+++ nn.cpp
@@ -1,37 +1,94 @@
 
-#include "../sgg/sgg.h"
+#include "../sgg/slib.h"
 
-struct NeuralNetwork
+struct neural_network
 {
     u32 InputCount;
     u32 OutputCount;
     u32 *LayerSizes;
+    u32 LayerCount;
     u32 MaximumIterations;
     r32 Beta;
     r32 Alpha;
     r32 Epsilon;
     r32 *Data;
+    r32 *Weights;
 };
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 internal r32
 Sigmoid(r32 X)
 {
-    return 0;
+    return 1.0f/(1.0f + (r32)pow(e32,-X));
+}
+
+internal void
+SeedArrayRandomly(r32 *Array, u32 ArraySize)
+{
+    for(u32 ArrayIndex = 0;
+        ArrayIndex < ArraySize;
+        ++ArrayIndex)
+    {
+        r32 RandomNumber = (r32)rand();
+        *(Array + ArrayIndex) = RandomNumber;
+    }
+}
+
+internal void
+PrintArray(r32 *Array, u32 ArraySize)
+{
+    printf("\n\nPrint Array\n\n");
+    for(u32 ArrayIndex = 0;
+        ArrayIndex < ArraySize;
+        ++ArrayIndex)
+    {
+        printf("float %d: %f\n", ArrayIndex, *Array);
+    }
+}
+
+internal void
+InitializeNeuralNetwork(neural_network *NeuralNetwork)
+{
+    
+    u32 DataArraySize = 0, WeightArraySize = 0, LayerSize = 0, PrevLayerSize = 0;
+    for(u32 LayerIndex = 0;
+        LayerIndex < NeuralNetwork->LayerCount;
+        ++LayerIndex)
+    {
+        LayerSize = *(NeuralNetwork->LayerSizes + LayerIndex);
+        DataArraySize += LayerSize;
+        WeightArraySize += LayerSize * PrevLayerSize + PrevLayerSize;
+        PrevLayerSize = LayerSize;
+    }
+
+    NeuralNetwork->Data = (r32*)malloc(sizeof(r32) * DataArraySize);
+    NeuralNetwork->Weights = (r32*)malloc(sizeof(r32) * WeightArraySize);
+    SeedArrayRandomly(NeuralNetwork->Data, DataArraySize);
+    SeedArrayRandomly(NeuralNetwork->Weights, WeightArraySize);
+
+    PrintArray(NeuralNetwork->Data, DataArraySize);
+    PrintArray(NeuralNetwork->Weights, WeightArraySize);
 }
 
 internal r32 *
-FeedForward(NeuralNetwork *nn, r32 *DataPoint)
+FeedForward(neural_network *NeuralNetwork, r32 *DataPoint)
 {
     return 0;
 }
 
 internal void
-BackPropogate(NeuralNetwork *nn, r32 *DataPoint)
+BackPropogate(neural_network *NeuralNetwork, r32 *DataPoint)
 {
 }
 
 s32 main()
 {
+    srand((u32)time(0));
+    
     r32 TrainingData[] = {0,0,0,0,
                          0,0,1,1,
                          0,1,0,1,
@@ -50,12 +107,13 @@ s32 main()
                      1,1,0,
                      1,1,1,};
     
-    NeuralNetwork xor = {};
+    neural_network xor = {};
     xor.InputCount = 3;
     xor.OutputCount = 1;
     
     u32 LayerSizes[] = {3,3,2,1};
     xor.LayerSizes = LayerSizes;
+    xor.LayerCount = ArrayCount(LayerSizes);
 
     xor.Beta = 0.3f;
     xor.Alpha = 0.1f;
@@ -68,7 +126,8 @@ s32 main()
 
     u32 TrainingDataPointCount = ArrayCount(TrainingData) / (xor.InputCount + xor.OutputCount);
     u32 TestDataPointCount = ArrayCount(TestData) / (xor.InputCount);
-    
+
+    InitializeNeuralNetwork(&xor);
 
     for(u32 IterationIndex = 0;
         IterationIndex < xor.MaximumIterations;
@@ -89,7 +148,7 @@ s32 main()
             OutputIndex < xor.OutputCount;
             ++OutputIndex)
         {
-            r32 OutputValue = *(NeuralNetworkOutput + OutputIndex);
+            //r32 OutputValue = *(NeuralNetworkOutput + OutputIndex);
         }
     }
     
