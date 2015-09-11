2f354884e57d5da0ca24a71cf1db7959811723ca
Steven Grissom <sggrissom@gmail.com>
2015-09-11 09:31:52 -0500

    add the slow flag to the build so asserts will fire

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set IgnoredWarnings= -wd4505 -wd4201 -wd4100 -wd4189
-set CommonCompilerFlags= -Ddebug=1 -MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -FC -Z7 %IgnoredWarnings%
+set CommonCompilerFlags= -Dslow=1 -Ddebug=1 -MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -FC -Z7 %IgnoredWarnings%
 set CommonLinkerFlags= -incremental:no -opt:ref
 
 IF NOT EXIST bin mkdir bin
