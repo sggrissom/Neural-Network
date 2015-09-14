bfef02742377a98623ba188067c1532f18380fde
Steven Grissom <sggrissom@gmail.com>
2015-09-14 16:18:47 -0500

    modified build.bat to use correct compiler flags for various build types.

--- build.bat
+++ build.bat
@@ -1,12 +1,14 @@
 @echo off
 
 set IgnoredWarnings= -wd4505 -wd4201 -wd4100 -wd4189
-set CommonCompilerFlags= -DSLOW=1 -DDEBUG=1 -MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -FC -Z7 %IgnoredWarnings%
+set CommonCompilerFlags= -DSLOW=1 -DDEBUG=1 -I w:\slib -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -FC -Z7 %IgnoredWarnings%
+set DebugCompilerFlags= -DSLOW=1 -DDEBUG=1 -MTd -Od %CommonCompilerFlags%
+set ReleaseCompilerFlags= -MT -O2 %CommonCompilerFlags%
 set CommonLinkerFlags= -incremental:no -opt:ref
 
 IF NOT EXIST bin mkdir bin
 pushd bin
 
-cl %CommonCompilerFlags% ..\src\nn.cpp /link %CommonLinkerFlags%
+cl %DebugCompilerFlags% ..\src\nn.cpp /link %CommonLinkerFlags%
 
 popd
