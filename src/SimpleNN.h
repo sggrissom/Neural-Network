e382f5ba0d15ca2978b0d98e9110c2f7fef7c287
Steven Grissom <sggrissom@gmail.com>
2015-09-21 17:01:08 -0500

    fixing the simple implementation and getting a good debug output. Now I need to compare this implementation to the flat one and see what's going wrong.

--- src/SimpleNN.h
+++ src/SimpleNN.h
@@ -12,7 +12,6 @@
 #include <time.h>
 #include <stdlib.h>
 
-
 class CBackProp{
 
 //	output of each neuron
@@ -26,11 +25,11 @@ class CBackProp{
 
 //	no of layers in net
 //	including input layer
-	u32 numl;
+	int numl;
 
 //	vector of numl elements for size 
 //	of each layer
-	u32 *lsize;
+	int *lsize;
 
 //	learning rate
 	r32 beta;
@@ -50,7 +49,7 @@ public:
 	~CBackProp();
 
 //	initializes and allocates memory
-	CBackProp(u32 nl,u32 *sz,r32 b,r32 a);
+	CBackProp(int nl,int *sz,r32 b,r32 a);
 
 //	backpropogates error for one set of input
 	void bpgt(r32 *in,r32 *tgt);
@@ -62,25 +61,26 @@ public:
 	r32 mse(r32 *tgt) const;	
 	
 //	returns i'th output of the net
-	r32 Out(u32 i) const;
+	r32 Out(int i) const;
 };
 
+
 //	initializes and allocates memory on heap
-CBackProp::CBackProp(u32 nl,u32 *sz,r32 b,r32 a):beta(b),alpha(a)
+CBackProp::CBackProp(int nl,int *sz,r32 b,r32 a):beta(b),alpha(a)
 {
-    u32 i = 0;
 
 	//	set no of layers and their sizes
 	numl=nl;
-	lsize=new u32[numl];
+	lsize=new int[numl];
 
-	for(u32 i=0;i<numl;i++){
+	for(int i=0;i<numl;i++){
 		lsize[i]=sz[i];
 	}
 
 	//	allocate memory for output of each neuron
 	out = new r32*[numl];
 
+    s32 i;
 	for( i=0;i<numl;i++){
 		out[i]=new r32[lsize[i]];
 	}
@@ -99,7 +99,7 @@ CBackProp::CBackProp(u32 nl,u32 *sz,r32 b,r32 a):beta(b),alpha(a)
 		weight[i]=new r32*[lsize[i]];
 	}
 	for(i=1;i<numl;i++){
-		for(u32 j=0;j<lsize[i];j++){
+		for(int j=0;j<lsize[i];j++){
 			weight[i][j]=new r32[lsize[i-1]+1];
 		}
 	}
@@ -112,7 +112,7 @@ CBackProp::CBackProp(u32 nl,u32 *sz,r32 b,r32 a):beta(b),alpha(a)
 
 	}
 	for(i=1;i<numl;i++){
-		for(u32 j=0;j<lsize[i];j++){
+		for(int j=0;j<lsize[i];j++){
 			prevDwt[i][j]=new r32[lsize[i-1]+1];
 		}
 	}
@@ -120,14 +120,14 @@ CBackProp::CBackProp(u32 nl,u32 *sz,r32 b,r32 a):beta(b),alpha(a)
 	//	seed and assign random weights
 	srand((unsigned)(time(NULL)));
 	for(i=1;i<numl;i++)
-		for(u32 j=0;j<lsize[i];j++)
-			for(u32 k=0;k<lsize[i-1]+1;k++)
+		for(int j=0;j<lsize[i];j++)
+			for(int k=0;k<lsize[i-1]+1;k++)
 				weight[i][j][k]=(r32)(rand())/(RAND_MAX/2) - 1;//32767
 
 	//	initialize previous weights to 0 for first iteration
 	for(i=1;i<numl;i++)
-		for(u32 j=0;j<lsize[i];j++)
-			for(u32 k=0;k<lsize[i-1]+1;k++)
+		for(int j=0;j<lsize[i];j++)
+			for(int k=0;k<lsize[i-1]+1;k++)
 				prevDwt[i][j][k]=(r32)0.0;
 
 // Note that the following variables are unused,
@@ -147,21 +147,20 @@ CBackProp::CBackProp(u32 nl,u32 *sz,r32 b,r32 a):beta(b),alpha(a)
 
 CBackProp::~CBackProp()
 {
-    u32 i = 0;
-    
 	//	free out
-	for(u32 i=0;i<numl;i++)
+	for(int i=0;i<numl;i++)
 		delete[] out[i];
 	delete[] out;
 
 	//	free delta
+    s32 i;
 	for(i=1;i<numl;i++)
 		delete[] delta[i];
 	delete[] delta;
 
 	//	free weight
 	for(i=1;i<numl;i++)
-		for(u32 j=0;j<lsize[i];j++)
+		for(int j=0;j<lsize[i];j++)
 			delete[] weight[i][j];
 	for(i=1;i<numl;i++)
 		delete[] weight[i];
@@ -169,7 +168,7 @@ CBackProp::~CBackProp()
 
 	//	free prevDwt
 	for(i=1;i<numl;i++)
-		for(u32 j=0;j<lsize[i];j++)
+		for(int j=0;j<lsize[i];j++)
 			delete[] prevDwt[i][j];
 	for(i=1;i<numl;i++)
 		delete[] prevDwt[i];
@@ -189,7 +188,7 @@ r32 CBackProp::sigmoid(r32 in)
 r32 CBackProp::mse(r32 *tgt) const
 {
 	r32 mse=0;
-	for(u32 i=0;i<lsize[numl-1];i++){
+	for(int i=0;i<lsize[numl-1];i++){
 		mse+=(tgt[i]-out[numl-1][i])*(tgt[i]-out[numl-1][i]);
 	}
 	return mse/2;
@@ -197,7 +196,7 @@ r32 CBackProp::mse(r32 *tgt) const
 
 
 //	returns i'th output of the net
-r32 CBackProp::Out(u32 i) const
+r32 CBackProp::Out(int i) const
 {
 	return out[numl-1][i];
 }
@@ -208,15 +207,16 @@ void CBackProp::ffwd(r32 *in)
 	r32 sum;
 
 	//	assign content to input layer
-	for(u32 i=0;i<lsize[0];i++)
+	for(int i=0;i<lsize[0];i++)
 		out[0][i]=in[i];  // output_from_neuron(i,j) Jth neuron in Ith Layer
 
 	//	assign output(activation) value 
 	//	to each neuron usng sigmoid func
-	for(u32 i=1;i<numl;i++){				// For each layer
-		for(u32 j=0;j<lsize[i];j++){		// For each neuron in current layer
+    s32 i;
+	for(i=1;i<numl;i++){				// For each layer
+		for(int j=0;j<lsize[i];j++){		// For each neuron in current layer
 			sum=0.0;
-			for(u32 k=0;k<lsize[i-1];k++){		// For input from each neuron in preceeding layer
+			for(int k=0;k<lsize[i-1];k++){		// For input from each neuron in preceeding layer
 				sum+= out[i-1][k]*weight[i][j][k];	// Apply weight to inputs and add to sum
 			}
 			sum+=weight[i][j][lsize[i-1]];		// Apply bias
@@ -236,17 +236,17 @@ void CBackProp::bpgt(r32 *in,r32 *tgt)
 	ffwd(in);
 
 	//	find delta for output layer
-	for(u32 i=0;i<lsize[numl-1];i++){
+	for(int i=0;i<lsize[numl-1];i++){
 		delta[numl-1][i]=out[numl-1][i]*
 		(1-out[numl-1][i])*(tgt[i]-out[numl-1][i]);
 	}
 
-    u32 i = 0;
+    s32 i;
 	//	find delta for hidden layers	
 	for(i=numl-2;i>0;i--){
-		for(u32 j=0;j<lsize[i];j++){
+		for(int j=0;j<lsize[i];j++){
 			sum=0.0;
-			for(u32 k=0;k<lsize[i+1];k++){
+			for(int k=0;k<lsize[i+1];k++){
 				sum+=delta[i+1][k]*weight[i+1][k][j];
 			}
 			delta[i][j]=out[i][j]*(1-out[i][j])*sum;
@@ -255,53 +255,42 @@ void CBackProp::bpgt(r32 *in,r32 *tgt)
 
 	//	apply momentum ( does nothing if alpha=0 )
 	for(i=1;i<numl;i++){
-		for(u32 j=0;j<lsize[i];j++){
-			for(u32 k=0;k<lsize[i-1];k++){
-				;//weight[i][j][k]+=alpha*prevDwt[i][j][k];
+		for(int j=0;j<lsize[i];j++){
+			for(int k=0;k<lsize[i-1];k++){
+				weight[i][j][k]+=alpha*prevDwt[i][j][k];
 			}
-			;//weight[i][j][lsize[i-1]]+=alpha*prevDwt[i][j][lsize[i-1]];
+			weight[i][j][lsize[i-1]]+=alpha*prevDwt[i][j][lsize[i-1]];
 		}
 	}
 
 	//	adjust weights usng steepest descent	
 	for(i=1;i<numl;i++){
-		for(u32 j=0;j<lsize[i];j++){
-			for(u32 k=0;k<lsize[i-1];k++){
-				weight[i][j][k]+=beta*delta[i][j]*out[i-1][k];
-				//weight[i][j][k]+=prevDwt[i][j][k];
+		for(int j=0;j<lsize[i];j++){
+			for(int k=0;k<lsize[i-1];k++){
+				prevDwt[i][j][k]=beta*delta[i][j]*out[i-1][k];
+				weight[i][j][k]+=prevDwt[i][j][k];
 			}
-			weight[i][j][lsize[i-1]]+=beta*delta[i][j];
-			//weight[i][j][lsize[i-1]]+=prevDwt[i][j][lsize[i-1]];
+			prevDwt[i][j][lsize[i-1]]=beta*delta[i][j];
+			weight[i][j][lsize[i-1]]+=prevDwt[i][j][lsize[i-1]];
 		}
 	}
 }
+
 
 internal void
 SimpleNN()
 {
 	// prepare XOR traing data
 	r32 data[][4]={
-				0,	0,	0,	0,
-				0,	0,	1,	1,
-				0,	1,	0,	1,
-				0,	1,	1,	0,
-				1,	0,	0,	1,
-				1,	0,	1,	0,
-				1,	1,	0,	0,
-				1,	1,	1,	1 };
+        0,	0,	0,	0,
+        0,	0,	1,	1,
+        0,	1,	0,	1,
+        0,	1,	1,	0,
+        1,	0,	0,	1,
+        1,	0,	1,	0,
+        1,	1,	0,	0,
+        1,	1,	1,	1 };
 
-	// prepare test data	
-	r32 testData[][3]={
-								0,      0,      0,
-                                0,      0,      1,
-                                0,      1,      0,
-                                0,      1,      1,
-                                1,      0,      0,
-                                1,      0,      1,
-                                1,      1,      0,
-                                1,      1,      1};
-
-	
 	// defining a net with 4 layers having 3,3,3, and 1 neuron respectively,
 	// the first layer is input layer i.e. simply holder for the input parameters
 	// and has to be the same size as the no of input parameters, in out example 3
@@ -315,11 +304,11 @@ SimpleNN()
 
 	
 	// maximum no of iterations during training
-	u32 num_iter = 20000000;
+	u32 num_iter = 2000000;
 
 	
 	// Creating the net
-	CBackProp *bp = new CBackProp(numLayers, lSz, beta, alpha);
+	CBackProp *bp = new CBackProp(numLayers, (int *)lSz, beta, alpha);
 	
 	for (u32 i=0; i<num_iter ; i++)
 	{
@@ -328,14 +317,17 @@ SimpleNN()
 
 	for (u32 i = 0 ; i < 8 ; i++ )
 	{
-		bp->ffwd(testData[i]);
-        for(u32 InputIndex = 0;
-            InputIndex < lSz[0];
-            ++InputIndex)
+		bp->ffwd(data[i]);
+        u32 output = (u32)(data[i%8][3]);
+        u32 rounded = (u32)(bp->Out(0) + 0.5f);
+        if(output==rounded)
         {
-            printf("%f ", testData[i][InputIndex]);
+            printf("success! -- ");
+        } else
+        {
+            printf("fail! -- ");
         }
-        printf("%f\n", bp->Out(0));
+        printf("prediction: %d -- target: %d\n", rounded, output);
 	}
 }
 
