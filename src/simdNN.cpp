2a861b74201dd94474cf999d37a0bcf7262d2c9b
Steven Grissom <sggrissom@gmail.com>
2015-12-16 01:46:44 -0600

    having some SIMD issues. woohoo.

--- src/simdNN.cpp
+++ src/simdNN.cpp
@@ -7,6 +7,112 @@
 #define ALIGN_SIZE 16
 #define ALIGN __declspec(align(ALIGN_SIZE))
 
+                __m128 One = _mm_set1_ps(1);
+
+
+/* declare some SSE constants -- why can't I figure a better way to do that? */
+#define _PS_CONST(Name, Val)                                            \
+  static const ALIGN16_BEG r32 _ps_##Name[4] ALIGN16_END = { Val, Val, Val, Val }
+#define _PI32_CONST(Name, Val)                                            \
+  static const ALIGN16_BEG s32 _pi32_##Name[4] ALIGN16_END = { Val, Val, Val, Val }
+#define _PS_CONST_TYPE(Name, Type, Val)                                 \
+  static const ALIGN16_BEG Type _ps_##Name[4] ALIGN16_END = { Val, Val, Val, Val }
+
+# define ALIGN16_BEG __declspec(align(16))
+# define ALIGN16_END
+
+_PS_CONST(1  , 1.0f);
+_PS_CONST(0p5, 0.5f);
+/* the smallest non denormalized float number */
+_PS_CONST_TYPE(min_norm_pos, int, 0x00800000);
+_PS_CONST_TYPE(mant_mask, int, 0x7f800000);
+_PS_CONST_TYPE(inv_mant_mask, int, ~0x7f800000);
+
+_PS_CONST_TYPE(sign_mask, int, (int)0x80000000);
+_PS_CONST_TYPE(inv_sign_mask, int, ~0x80000000);
+
+_PI32_CONST(1, 1);
+_PI32_CONST(inv1, ~1);
+_PI32_CONST(2, 2);
+_PI32_CONST(4, 4);
+_PI32_CONST(0x7f, 0x7f);
+
+_PS_CONST(cephes_SQRTHF, 0.707106781186547524f);
+_PS_CONST(cephes_log_p0, 7.0376836292E-2f);
+_PS_CONST(cephes_log_p1, - 1.1514610310E-1f);
+_PS_CONST(cephes_log_p2, 1.1676998740E-1f);
+_PS_CONST(cephes_log_p3, - 1.2420140846E-1f);
+_PS_CONST(cephes_log_p4, + 1.4249322787E-1f);
+_PS_CONST(cephes_log_p5, - 1.6668057665E-1f);
+_PS_CONST(cephes_log_p6, + 2.0000714765E-1f);
+_PS_CONST(cephes_log_p7, - 2.4999993993E-1f);
+_PS_CONST(cephes_log_p8, + 3.3333331174E-1f);
+_PS_CONST(cephes_log_q1, -2.12194440e-4f);
+_PS_CONST(cephes_log_q2, 0.693359375f);
+
+_PS_CONST(exp_hi,	88.3762626647949f);
+_PS_CONST(exp_lo,	-88.3762626647949f);
+
+_PS_CONST(cephes_LOG2EF, 1.44269504088896341f);
+_PS_CONST(cephes_exp_C1, 0.693359375f);
+_PS_CONST(cephes_exp_C2, -2.12194440e-4f);
+
+_PS_CONST(cephes_exp_p0, 1.9875691500E-4f);
+_PS_CONST(cephes_exp_p1, 1.3981999507E-3f);
+_PS_CONST(cephes_exp_p2, 8.3334519073E-3f);
+_PS_CONST(cephes_exp_p3, 4.1665795894E-2f);
+_PS_CONST(cephes_exp_p4, 1.6666665459E-1f);
+_PS_CONST(cephes_exp_p5, 5.0000001201E-1f);
+
+__m128 exp_ps(__m128 x) {
+  __m128 tmp = _mm_setzero_ps(), fx;
+  __m128i emm0;
+  __m128 one = *(__m128*)_ps_1;
+
+  x = _mm_min_ps(x, *(__m128*)_ps_exp_hi);
+  x = _mm_max_ps(x, *(__m128*)_ps_exp_lo);
+
+  /* express exp(x) as exp(g + n*log(2)) */
+  fx = _mm_mul_ps(x, *(__m128*)_ps_cephes_LOG2EF);
+  fx = _mm_add_ps(fx, *(__m128*)_ps_0p5);
+
+  emm0 = _mm_cvttps_epi32(fx);
+  tmp  = _mm_cvtepi32_ps(emm0);
+  /* if greater, substract 1 */
+  __m128 mask = _mm_cmpgt_ps(tmp, fx);    
+  mask = _mm_and_ps(mask, one);
+  fx = _mm_sub_ps(tmp, mask);
+
+  tmp = _mm_mul_ps(fx, *(__m128*)_ps_cephes_exp_C1);
+  __m128 z = _mm_mul_ps(fx, *(__m128*)_ps_cephes_exp_C2);
+  x = _mm_sub_ps(x, tmp);
+  x = _mm_sub_ps(x, z);
+
+  z = _mm_mul_ps(x,x);
+  
+  __m128 y = *(__m128*)_ps_cephes_exp_p0;
+  y = _mm_mul_ps(y, x);
+  y = _mm_add_ps(y, *(__m128*)_ps_cephes_exp_p1);
+  y = _mm_mul_ps(y, x);
+  y = _mm_add_ps(y, *(__m128*)_ps_cephes_exp_p2);
+  y = _mm_mul_ps(y, x);
+  y = _mm_add_ps(y, *(__m128*)_ps_cephes_exp_p3);
+  y = _mm_mul_ps(y, x);
+  y = _mm_add_ps(y, *(__m128*)_ps_cephes_exp_p4);
+  y = _mm_mul_ps(y, x);
+  y = _mm_add_ps(y, *(__m128*)_ps_cephes_exp_p5);
+  y = _mm_mul_ps(y, z);
+  y = _mm_add_ps(y, x);
+  y = _mm_add_ps(y, one);
+
+  emm0 = _mm_cvttps_epi32(fx);
+  emm0 = _mm_add_epi32(emm0, *(__m128i*)_pi32_0x7f);
+  emm0 = _mm_slli_epi32(emm0, 23);
+  __m128 pow2n = _mm_castsi128_ps(emm0);
+  y = _mm_mul_ps(y, pow2n);
+  return y;
+}
+
 void PrintWide(__m128 WideFloat)
 {
     r32 *Floats = (r32*) &WideFloat;
@@ -70,25 +176,10 @@ FeedForward(neural_network *NeuralNetwork, r32 *DataPoint)
                 r32 Sum3 = 0.0f;
                 __m128 WideSum3 = _mm_set1_ps(0.0f);
                 
-                r32 c00 = 0.0f;
-                r32 c01 = 0.0f;
-                r32 c02 = 0.0f;
-                r32 c03 = 0.0f;
-
-                r32 c10 = 0.0f;
-                r32 c11 = 0.0f;
-                r32 c12 = 0.0f;
-                r32 c13 = 0.0f;
-
-                r32 c20 = 0.0f;
-                r32 c21 = 0.0f;
-                r32 c22 = 0.0f;
-                r32 c23 = 0.0f;
-
-                r32 c30 = 0.0f;
-                r32 c31 = 0.0f;
-                r32 c32 = 0.0f;
-                r32 c33 = 0.0f;
+                r32 c0 = 0.0f;
+                r32 c1 = 0.0f;
+                r32 c2 = 0.0f;
+                r32 c3 = 0.0f;
 
                 {
                     u32 PrevNeuronIndex = 0;
@@ -145,30 +236,44 @@ FeedForward(neural_network *NeuralNetwork, r32 *DataPoint)
                         PrevNeuronIndex < LayerSizes[LayerIndex-1];
                         ++PrevNeuronIndex)
                     {
-                        c00 += (Data[DataRowPtr[LayerIndex-1]+PrevNeuronIndex]) *
+                        InvalidCodePath;
+                        
+                        c0 += (Data[DataRowPtr[LayerIndex-1]+PrevNeuronIndex]) *
                             (Weights[WeightsRowPtr[LayerIndex] +
                                      (PrevLayerSize)*(NeuronIndex+0) +
                                      PrevNeuronIndex]);
-                        c10 += (Data[DataRowPtr[LayerIndex-1]+PrevNeuronIndex]) *
+                        c1 += (Data[DataRowPtr[LayerIndex-1]+PrevNeuronIndex]) *
                             (Weights[WeightsRowPtr[LayerIndex] +
                                      (PrevLayerSize)*(NeuronIndex+1) +
                                      PrevNeuronIndex]);
-                        c20 += (Data[DataRowPtr[LayerIndex-1]+PrevNeuronIndex]) *
+                        c2 += (Data[DataRowPtr[LayerIndex-1]+PrevNeuronIndex]) *
                             (Weights[WeightsRowPtr[LayerIndex] +
                                      (PrevLayerSize)*(NeuronIndex+2) +
                                      PrevNeuronIndex]);
-                        c30 += (Data[DataRowPtr[LayerIndex-1]+PrevNeuronIndex]) *
+                        c3 += (Data[DataRowPtr[LayerIndex-1]+PrevNeuronIndex]) *
                             (Weights[WeightsRowPtr[LayerIndex] +
                                      (PrevLayerSize)*(NeuronIndex+3) +
                                      PrevNeuronIndex]);
                     }
                 }
-                
-                _mm_store_ps(Result, WideSum0);
-                _mm_store_ps(Result+4, WideSum1);
-                _mm_store_ps(Result+8, WideSum2);
-                _mm_store_ps(Result+12, WideSum3);
 
+                __m128 WideSum01 = _mm_hadd_ps(WideSum1, WideSum0);
+                __m128 WideSum23 = _mm_hadd_ps(WideSum3, WideSum2);
+
+                __m128 WideSumTotal = _mm_hadd_ps(WideSum23, WideSum01);
+
+                WideSumTotal = _mm_shuffle_ps(WideSumTotal, WideSumTotal, _MM_SHUFFLE(0,1,2,3));
+
+                __m128 Leftovers = _mm_load_ps(Weights + WeightsRowPtr[LayerIndex] +
+                                               PrevLayerSize * NeuronIndex + LayerSizes[LayerIndex-1]);
+
+                WideSumTotal = _mm_add_ps(WideSumTotal, Leftovers);
+                
+                __m128 SigmoidResult = _mm_div_ps(One, _mm_add_ps(One, exp_ps(WideSumTotal)));
+
+                _mm_store_ps(Data+DataRowPtr[LayerIndex]+(NeuronIndex), SigmoidResult);
+                
+                /*
                 Sum0 += c00 +
                     Result[0] + Result[1] + Result[2] + Result[3] +
                     Weights[WeightsRowPtr[LayerIndex] +
@@ -200,7 +305,7 @@ FeedForward(neural_network *NeuralNetwork, r32 *DataPoint)
                             LayerSizes[LayerIndex-1]];
 
                 Data[DataRowPtr[LayerIndex]+(NeuronIndex+3)] = (1.0f/(1.0f+(r32)exp(-Sum3)));
-
+                */
             }
             
             free(Result);
